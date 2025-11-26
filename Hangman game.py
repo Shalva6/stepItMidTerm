a19e526776f35fd922a05edf313f322ec748e1d0
BL220 <bacho.lomidze2004@gmail.com>
2025-11-26 18:32:13 +0400

    Add files via upload

---
+++ Hangman game.py
@@ -0,0 +1,69 @@
+import random
+
+class Hangman:
+    def __init__(self):
+        # სიტყვების სია
+        self.words_list = ["PYTHON","PYCHARM","ITSTEP"]
+
+
+        self.secret_word = random.choice(self.words_list)
+
+
+        self.lives = 6  # სიცოცხლე
+        self.guesses = []  # გამოყენებული ასოების სია
+
+    def show_board(self):
+
+        display_list = []
+
+
+        for char in self.secret_word:
+            if char in self.guesses:
+                # თუ ასო უკვე გამოცნობილია, ვამატებთ ასოს
+                display_list.append(char)
+            else:
+                # თუ არა, ვამატებთ ტირეს
+                display_list.append("_")
+
+
+        print("სიტყვა:", " ".join(display_list))
+
+        # თუ სიაში ტირე აღარ დარჩა, ესეიგი მოიგო
+        if "_" not in display_list:
+            return True
+        return False
+
+    def play(self):
+        """თამაშის მთავარი ციკლი"""
+        print("დავიწყეთ თამაში!")
+
+        # სანამ სიცოცხლე გვაქვს
+        while self.lives > 0:
+            print("\n----------------")
+            print(f"დარჩენილი სიცოცხლე: {self.lives}")
+
+            # ვამოწმებთ, ხომ არ მოიგო
+            if self.show_board():
+                print("გილოცავ! შენ გამოიცანი სიტყვა!")
+                return  #თამაში მორჩა
+
+            # ასოს შეყვანა
+            letter = input("შეიყვანე ასო: ").upper()  #ვაქცევთ დიდ ასოდ
+
+            #ვამატებთ ასოს გამოყენებულებში
+            self.guesses.append(letter)
+
+            if letter in self.secret_word:
+                print("სწორია!")
+            else:
+                print("არასწორია!")
+                self.lives = self.lives - 1
+
+        # თუ ციკლი დამთავრდა და აქ ჩამოვიდა, ე.ი. სიცოცხლე გათავდა
+        print("\nსამწუხაროდ წააგეთ.")
+        print(f"სწორი სიტყვა იყო: {self.secret_word}")
+
+
+
+game = Hangman()
+game.play()
